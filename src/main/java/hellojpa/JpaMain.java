83d67e40e8ce34e44e8d88fa9414d74d0d46113b
nayeon-jo <nyc0516@naver.com>
2023-09-15 14:27:24 +0900

    Hello JPA - 애플리케이션 개발

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,9 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
-
     public static void main(String[] args){
 
         EntityManagerFactory emf=Persistence.createEntityManagerFactory("hello");
@@ -18,18 +18,20 @@ public class JpaMain {
         EntityTransaction tx=em.getTransaction();
         tx.begin();
 
+        //Member findMember=em.find(Member.class, 1l);
         try{
-            Member member=new Member();
-            member.setId(2L);
-            member.setName("helloB");
+            List<Member> result=em.createQuery("select m from Member as m", Member.class)
+                            .getResultList();
 
-            em.persist(member);
+            for(Member member : result){
+                System.out.println("member.name = " +member.getName());
+            }
 
-            tx.commit();
+            tx.commit();    //커밋해줘야 반영된다
         }catch (Exception e){
             tx.rollback();
         }finally {
-            em.close();     //사용끝난후, 꼭 닫아줘야함
+            em.close();     //사용끝난후, 꼭 닫아줘야함 (그래야 반환된다)
         }
         /***/
 
